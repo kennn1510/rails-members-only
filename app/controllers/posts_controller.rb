069b2e36483e5a16182899b7a8b4bd0f798d16ae
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-07-27 23:26:47 -0500

    Modify post create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: "Post was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
