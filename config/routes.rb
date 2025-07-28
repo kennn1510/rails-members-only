afa58ebf40312e983213ed0f0ef562d1e5ecfc10
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-07-27 23:09:51 -0500

    Do db migration and add new post view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  resources :posts, only: [ :new, :create, :index ]
 
-  resources :posts, only: [:new, :create, :index]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -13,5 +12,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
 end
